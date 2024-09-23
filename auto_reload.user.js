9d5948aa8b5c2bb34c1f4ba0a5067f30fc5f2b90
Taylor <78652522+Taylor-eOS@users.noreply.github.com>
2024-09-23 17:01:03 +0200

    Reloads only main page

--- auto_reload.user.js
+++ auto_reload.user.js
@@ -4,19 +4,17 @@
 // @version      1.2
 // @description  Reload the web app shortly after access
 // @author       YourName
-// @match        *.chatgpt.com/*
+// @match        https://chatgpt.com/
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
-
     if (!sessionStorage.getItem('reloaded')) {
         sessionStorage.setItem('reloaded', 'true');
-
         setTimeout(function() {
-            console.log('Reloading the page after access...');
+            console.log('Reloading the page');
             window.location.reload();
-        }, 500);
+        }, 400);
     }
 })();
